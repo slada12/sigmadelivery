6763d6abaf8cfbc86f07f62cb3378d95ae76bde3
Emmanuel Worgu <emmanuelworgu@gmail.com>
2021-08-30 23:31:41 +0100

    fixed cors policy

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const loginRoute = require('./Routes/admin');
 const app = express();
 const port = process.env.PORT || 5000;
 const corsOption = {
-  origin: ['http://localhost:3000', 'https://sigmadelivery-frontend.pages.dev'],
+  origin: ['http://localhost:3000', 'https://sigmadelivery-frontend.pages.dev', 'https://sladatransport.com'],
 };
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,6 +18,10 @@ app.use(cors(corsOption));
 
 app.use('/track', mainRoute);
 app.use('/admin', loginRoute);
+
+app.get('/', (req, res) => {
+  res.send('The Site is Up and Running');
+});
 
 app.listen(port, () => {
   console.log(`Server Started on port ${port}`);
