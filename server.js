191e8324ef1fcfb15411358d4bdbe042c2e7c1a0
Emmanuel Worgu <emmanuelworgu@gmail.com>
2021-08-30 23:48:06 +0100

    added cron job

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const cron = require('node-cron');
 
 const mainRoute = require('./Routes/main');
 const loginRoute = require('./Routes/admin');
@@ -26,3 +27,53 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server Started on port ${port}`);
 });
+
+(function serverWorker() {
+  cron.schedule('*/10 * * * *', async () => {
+    console.log('Trying to hit the API');
+    const date = new Date();
+    const hours = date.getUTCHours();
+    console.log(hours);
+
+    if (hours > 6) {
+      const options = {
+        method: 'GET',
+        url: 'https://sigmadelivery.herokuapp.com/',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      };
+      console.log('Hit the API');
+
+      const response = await axios(options);
+      // console.log(response.data);
+
+      // const portCondition = port === 5000 ? 'Relaks Development Robot' : 'Relaks Robot';
+
+      // if (response.status === 200) {
+      //   const mailSend = {
+      //     from: process.env.Email,
+      //     to: process.env.User,
+      //     subject: 'Your site is still Online',
+      //     html: `
+      //     <h4>Hello Admin,</h4>
+      //       Your site is still online. I will continually check the site every ten minutes to make sure everything is working fine. 
+      //     <h4><b>${portCondition}</b></h4>`,
+      //   };
+
+      //   admin_transporter.sendMail(mailSend, (err, info) => {
+      //     if (err) {
+      //       console.log(chalk.red(`The Admin with email ${process.env.admin_email} did not recieved the mail: ${err.response}`));
+      //     } else {
+      //       console.log(chalk.green(`The message was sent ${info.response}`));
+      //     }
+      //   });
+      // } else {
+      //   return null;
+      // }
+    } else {
+      return null;
+    }
+  });
+}());
