e4aaf16a42b8f8f1e8673dcd5d51ad2e67bf5ece
slada12 <104027817+slada12@users.noreply.github.com>
2025-04-17 00:49:42 -0400

    Update and rename emmanuelworgu@gmail.com.js to Duanedillard888@gmail.com.js

---
+++ Duanedillard888@gmail.com.js
@@ -0,0 +1,1 @@
+{"email":"Duanedillard888@gmail.com","pass":"duanedillard-1234567","token":"emmanuelworgu@gmail.com-259261"}
--- emmanuelworgu@gmail.com.js
+++
@@ -1,1 +0,0 @@
-{"email":"dan@admin","pass":"dan-1234567","token":"emmanuelworgu@gmail.com-259261"}
